e307fe4867c19c7e2a9a12a42c24d12a2bbf5222
rzeznikm <ironmichal92@gmail.com>
2017-10-28 23:43:33 +0200

    Few Kebab added

--- app/src/main/java/kebaza/com/kebaza/BestKebebFinder.java
+++ app/src/main/java/kebaza/com/kebaza/BestKebebFinder.java
@@ -10,6 +10,10 @@ public class BestKebebFinder {
 
     KebabPlace tanti = new KebabPlace("Tanti", 50.0756986f, 19.907227f, Size.medium);
     KebabPlace szwagier = new KebabPlace("Szwgier", 50.0658696f,19.9307775f, Size.big);
+    KebabPlace habibi = new KebabPlace("Habibi",  50.026696f , 19.7902979f, Size.medium);
+    KebabPlace laMarsa = new KebabPlace("La Marsa", 50.038007f, 19.9391413f, Size.small);
+    KebabPlace pod13 = new KebabPlace( "Kebab pod 13", 50.0482559f, 19.9418603f, Size.big);
+    KebabPlace misterKebab = new KebabPlace( "MisterKebab", 50.0627687f,19.9324045f, Size.small);
 
     public KebabPlace findBestKebab(Size size)
     {
@@ -24,4 +28,5 @@ public class BestKebebFinder {
         }
         return place;
     }
+
 }
