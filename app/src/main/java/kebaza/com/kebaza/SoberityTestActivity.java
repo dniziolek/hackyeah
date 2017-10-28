a798b5e725626293b34565a3e789ff1800752c53
Dominika Niziolek <dominikaniziolek@213-17-157-195.static.ip.netia.com.pl>
2017-10-28 22:07:34 +0200

    added new logo and begginings of soberity test

--- app/src/main/java/kebaza/com/kebaza/SoberityTestActivity.java
+++ app/src/main/java/kebaza/com/kebaza/SoberityTestActivity.java
@@ -3,6 +3,12 @@ package kebaza.com.kebaza;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SoberityTestActivity extends AppCompatActivity {
 
@@ -10,7 +16,15 @@ public class SoberityTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soberity_test);
+        ButterKnife.bind(this);
 
         startActivity(new Intent(this, KebabSettingsActivity.class));
+
+
+    }
+
+    @OnClick(R.id.soberButton)
+    public void submit() {
+        Toast.makeText(this, "message test", Toast.LENGTH_LONG).show();
     }
 }
